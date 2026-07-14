5990b05e11ca252749473d8c2cf1a8151caf0125
Jihye-kr <jihyeg29@gmail.com>
2026-07-14 11:23:36 +0900

    fix: #76 질문 내역 컨텍스트 메뉴 뷰포트 고정(스크롤 시 메뉴 오프스크린 방지)

--- src/features/question/components/questions-list-page-content.tsx
+++ src/features/question/components/questions-list-page-content.tsx
@@ -79,12 +79,14 @@ function QuestionsListPageContent() {
       </main>
 
       {menuFor != null && (
-        <ChatContextMenu
-          dimmed
-          items={menuItems}
-          onDismiss={() => setMenuFor(null)}
-          className="left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-        />
+        <div className="fixed inset-0 z-50">
+          <ChatContextMenu
+            dimmed
+            items={menuItems}
+            onDismiss={() => setMenuFor(null)}
+            className="left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
       )}
 
       <ConfirmDialog
